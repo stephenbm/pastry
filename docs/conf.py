edc426f124ab92612075cf826a614edc6080d202
Stephen Breyer-Menke <Stephen.Breyer-Menke@standardbank.co.za>
2017-02-08 09:08:55 +0200

    Add os to setup.py and add abspath to sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+_d = os.path.dirname
+sys.path.insert(0, _d(_d(os.path.abspath(__file__))))
 
 
 # -- General configuration ------------------------------------------------
