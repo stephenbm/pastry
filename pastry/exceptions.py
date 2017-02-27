22b186ab537aa177da3e090d24d61b9d10549133
Stephen Breyer-Menke <Stephen.Breyer-Menke@standardbank.co.za>
2017-02-27 23:37:12 +0200

    Add docstring for exceptions

--- pastry/exceptions.py
+++ pastry/exceptions.py
@@ -1,7 +1,18 @@
+'''exceptions for pastry events'''
+
 class HttpError(Exception):
+    '''
+    Exception calling http request -adds teh ststus code)
+    '''
     def __init__(self, message, statuscode, *args, **kwargs):
+        '''
+        Create the Exception
+        '''
         self.statuscode = statuscode
         super(HttpError, self).__init__(message, *args, **kwargs)
 
     def __str__(self):
+        '''
+        Show the exception and statuscode
+        '''
         return '(%s) %s' % (self.statuscode, self.message)
