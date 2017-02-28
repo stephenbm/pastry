4c17ecd36df3fcefe9c62917041117924ce54114
Stephen Breyer-Menke <Stephen.Breyer-Menke@standardbank.co.za>
2017-02-28 10:52:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ tests_require = [
 
 setup(
     name='pastry',
-    version='0.1.7',
+    version='0.1.8',
     description='A simple api wrapper for chef',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Stephen Breyer-Menke',
